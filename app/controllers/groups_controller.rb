ab3c48a1596d292336fdffbaded17637a9aeddfa
supster <supatra.indhara@gmail.com>
2013-05-04 18:49:27 -0400

    add addresses to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,16 +7,31 @@ class GroupsController < ApplicationController
 
   	def new
   		@group = Group.new
+  		@origin_address = Address.new
+  		@destination_address = Address.new
   	end
 
   	def create
-	    @group = Group.new(params[:new_group])
-	    @user = current_user
+	    @group = Group.new(params[:group])
+	    #@group.users.build(current_user)
+	   	
+	   	@origin_address = Address.create(params[:origin_address])
+		@destination_address = Address.create(params[:destination_address])
+
+	    @group.origin_address_id = @origin_address.id
+	    @group.destination_address_id = @destination_address.id
+
 	    if @group.save
 	      flash[:success] = "Awesome, your a group owner!"
 	      redirect_to @group
 	    else
 	      render "new"      
 	    end
+  	end
+
+  	def show
+   		@group = Group.find(params[:id])
+ 		@origin_address = Address.find(@group.origin_address_id)
+  		@destination_address = Address.find(@group.destination_address_id)
   	end
 end
