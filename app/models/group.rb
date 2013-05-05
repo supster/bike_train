3792e6f82a5426698e3d3547650edec086308657
supster <supatra.indhara@gmail.com>
2013-05-05 11:55:27 -0400

    group validate

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,4 +6,8 @@ class Group < ActiveRecord::Base
   attr_accessible :groupMemberships_attributes
   
   accepts_nested_attributes_for :groupMemberships
+
+  validates :name, presence: true, length: { maximum: 50}
+  validates :origin_address_id, presence: true
+  validates :destination_address_id, presence: true
 end
