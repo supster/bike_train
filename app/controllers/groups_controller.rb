b7bb83435977605f1bc17561c4ea79fe312c210f
raj kandathi <raj.kandati@gmail.com>
2013-05-05 11:02:28 -0400

    working relationship between groups and users

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,6 +8,8 @@ class GroupsController < ApplicationController
   		@group = Group.new
   		@origin_address = Address.new
   		@destination_address = Address.new
+  		@group.users.build
+  		@group.groupMemberships.build
   	end
 
   	def create
@@ -16,14 +18,14 @@ class GroupsController < ApplicationController
 	    logger.info @group
 	    logger.info 'Printing users'
 	    logger.info current_user
-	    @group.users << current_user
+	    #@group.users << User.find(current_user.id)
 	   	
 	   	@origin_address = Address.create(params[:origin_address])
 		@destination_address = Address.create(params[:destination_address])
 
 	    @group.origin_address_id = @origin_address.id
 	    @group.destination_address_id = @destination_address.id
-	    
+
 	    if @group.save
 	      
 	      flash[:success] = "Awesome, your a group owner!"
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,8 @@ class Group < ActiveRecord::Base
   attr_accessible :name, :description, :destination_address_id, :origin_address_id
   has_many :groupMemberships
   has_many :users, :through => :groupMemberships 
+
+  attr_accessible :groupMemberships_attributes
+  
+  accepts_nested_attributes_for :groupMemberships
 end
